674d8089fff9734b986dc3a8417536a037bded48
Ugyen Tshering <tsheringugyen483@gmail.com>
2024-04-16 12:37:31 +0600

    ddd

--- contracts/SubjectList.sol
+++ contracts/SubjectList.sol
@@ -1,8 +1,9 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 contract SubjectList {
     uint256 public subjectsCount = 0;
+    mapping(string => uint) private codeToId;
 
     struct Subject {
         uint _id;
@@ -13,34 +14,29 @@ contract SubjectList {
 
     mapping(uint => Subject) public subjects;
 
-    event createSubjectEvent(
-        uint _id,
-        string indexed code,
-        string name,
-        bool retired
-    );
-    event markRetirdEvent(uint indexed cid);
-
-    constructor() {
-        // hhhhhhhhhhhhhhhhhhh
-    }
+    event SubjectCreated(uint _id, string code, string name, bool retired);
+    event SubjectRetired(uint _id);
 
     function createSubject(
         string memory _code,
         string memory _name
-    ) public returns (Subject memory) {
+    ) public returns (uint) {
         subjectsCount++;
         subjects[subjectsCount] = Subject(subjectsCount, _code, _name, false);
-        return subjects[subjectsCount];
+        codeToId[_code] = subjectsCount;
+        emit SubjectCreated(subjectsCount, _code, _name, false);
+        return subjectsCount; // Returns the ID of the new subject
     }
 
-    function markRetired(uint _id) public returns (Subject memory) {
+    function markRetired(string memory _code) public {
+        uint _id = codeToId[_code];
         subjects[_id].retired = true;
-        emit markRetirdEvent(subjectsCount);
-        return subjects[_id];
+        emit SubjectRetired(_id);
     }
 
-    function findSubject(uint _id) public view returns (Subject memory) {
-        return subjects[_id];
+    function findSubject(
+        string memory _code
+    ) public view returns (Subject memory) {
+        return subjects[codeToId[_code]];
     }
 }
